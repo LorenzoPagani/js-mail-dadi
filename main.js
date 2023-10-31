8074c6d1f281b80ef4ac1fdb390b6ab3af1b0364
Lorenzo Pagani <larrypg2@gmail.com>
2023-10-31 17:13:10 +0100

    Added dice game

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* email checker */
+
 const authorizedMail = ["ginopilotino@gmail.com", "fabbricadeimostri@gig.com", "emiglio@robot.com", "powerranger@blu.com"];
 console.log("list", authorizedMail);
 
@@ -11,16 +13,48 @@ button.addEventListener("click", function () {
     }
     if (authorizedMail.includes(emailInput)) {
         checkResult.classList = "text-success";
-        checkResult.innerHTML = "Accesso autorizzato!"
+        checkResult.innerHTML = "Accesso autorizzato!";
     }
     else if (!emailInput.includes("@")) {
         checkResult.classList = "text-danger";
-        checkResult.innerHTML = "not an email"
+        checkResult.innerHTML = "not an email";
     }
     else {
-        checkResult.innerHTML = "non autorizzato"
+        checkResult.innerHTML = "non autorizzato";
     }
 })
 
 
+/* lancio dadi */
+
+const diceButton = document.getElementById("dice-button");
+const yourRoll = document.getElementById("your-roll");
+const pcRoll = document.getElementById("pc-roll");
+const gameOutcome = document.getElementById("game-outcome");
+const diceSound = document.getElementById("diceSound");
+
+
+diceButton.addEventListener('click', function() {
+    diceSound.play();
+    let randomDice = Math.floor(Math.random() * 6 + 1);
+    yourRoll.innerHTML = randomDice;
+    randomDice = Math.floor(Math.random() * 6 + 1);
+    pcRoll.innerHTML = randomDice;
+
+    if (yourRoll.innerHTML > pcRoll.innerHTML) {
+        gameOutcome.innerHTML = "Hai Vinto!";
+        gameOutcome.classList = "text-success";
+    }
+
+    else if (yourRoll.innerHTML < pcRoll.innerHTML) {
+        gameOutcome.innerHTML = "Hai Perso!";
+        gameOutcome.classList = "text-danger";
+    } 
+    
+    else {
+        gameOutcome.innerHTML = "Pareggio!";
+        gameOutcome.classList = "";
+
+    }
+})
 
